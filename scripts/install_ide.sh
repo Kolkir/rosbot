60274bb4324d6417addd4993d8fb5d306b35e5bb
kolkir <rotate@ukr.net>
2020-12-15 16:23:27 +0000

    Add script to install development packages

---
+++ scripts/install_ide.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+apt-get update
+apt-get install -y qtcreator
+apt-get install -y libclang-common-8-dev
+apt-get install -y vim
+apt-get install -y tmux
+apt-get install -y git
+apt-get install -y git-gui
+apt-get install -y ros-noetic-cv-camera
+apt-get install -y ros-noetic-teleop-twist-keyboard
+apt-get install -y ros-noetic-rosparam-shortcuts
