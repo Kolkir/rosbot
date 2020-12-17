d5f95c48d258829db7d5a4d61d3d91d0e330d3f9
kolkir <rotate@ukr.net>
2020-12-17 12:53:06 +0000

    Start real motores HW interface implementation

---
+++ bot_hardware_interface/src/byj_steppers_hw.cpp
@@ -0,0 +1,6 @@
+#include "byj_steppers_hw.h"
+
+BYJSteppersHW::BYJSteppersHW()
+{
+
+}
---
+++ bot_hardware_interface/src/byj_steppers_hw.h
@@ -0,0 +1,11 @@
+#ifndef BYJSTEPPERSHW_H
+#define BYJSTEPPERSHW_H
+
+
+class BYJSteppersHW
+{
+public:
+  BYJSteppersHW();
+};
+
+#endif // BYJSTEPPERSHW_H
