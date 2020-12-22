f3763a48553b94cf93029049621b75f26c5d43e1
kolkir <rotate@ukr.net>
2020-12-22 10:57:20 +0000

    Update GPIO library to gpiod

--- bot_hardware_interface/src/gpio_opi.cpp
+++ bot_hardware_interface/src/gpio_opi.cpp
@@ -1,11 +1,16 @@
 #include "gpio_opi.h"
 
+#include <gpiod.h>
 #include <ros/ros.h>
-#include <wiringPi.h>
 #include <cassert>
 
-GPIO_OPI::GPIO_OPI() {
-  wiringPiSetupGpio();
+GPIO_OPI::GPIO_OPI() : chip_(nullptr, [](gpiod_chip*) {}) {
+  auto* chip = gpiod_chip_open_by_name(gpio_chip_name_.data());
+  if (!chip) {
+    ROS_ERROR_STREAM("Failed to open GPIO chip : " << gpio_chip_name_);
+    ros::shutdown();
+  }
+  chip_ = ChipPtr(chip, [](gpiod_chip* chip) { gpiod_chip_close(chip); });
 }
 
 GPIO_OPI::~GPIO_OPI() {}
@@ -13,15 +18,32 @@ GPIO_OPI::~GPIO_OPI() {}
 void GPIO_OPI::Output(const std::vector<size_t>& pins,
                       const std::vector<size_t>& values) {
   assert(pins.size() == values.size());
-
   size_t len = pins.size();
   for (size_t i = 0; i < len; ++i) {
-    digitalWrite(pins[i], values[i] >= 1 ? HIGH : LOW);
+    auto pin_num = pins[i];
+    auto ret = gpiod_line_set_value(pins_.at(pin_num).get(), values[i]);
+    if (ret < 0) {
+      ROS_ERROR_STREAM("Set line output failed\n");
+      ros::shutdown();
+    }
   }
 }
 
 void GPIO_OPI::ConfigureOutputPints(const std::vector<size_t>& pins) {
-  for (auto pin : pins) {
-    pinMode(pin, OUTPUT);
+  pins_.clear();
+  for (auto pin_num : pins) {
+    auto* pin = gpiod_chip_get_line(chip_.get(), pin_num);
+    if (!pin) {
+      ROS_ERROR_STREAM("Failed to open GPIO pin  : " << pin_num);
+      ros::shutdown();
+    }
+    pins_.emplace(
+        pin_num, PinPtr(pin, [](gpiod_line* pin) { gpiod_line_release(pin); }));
+
+    auto ret = gpiod_line_request_output(pin, "Consumer", 0);
+    if (ret < 0) {
+      ROS_ERROR_STREAM("Request GPIO pin " << pin_num << " as output failed\n");
+      ros::shutdown();
+    }
   }
 }
--- bot_hardware_interface/src/gpio_opi.h
+++ bot_hardware_interface/src/gpio_opi.h
@@ -1,20 +1,32 @@
 #ifndef GPIO_OPI_H
 #define GPIO_OPI_H
 
+#include <memory>
+#include <string>
+#include <unordered_map>
 #include "gpio_base.h"
+
+struct gpiod_chip;
+struct gpiod_line;
 
 class GPIO_OPI : public GPIOBase {
  public:
   GPIO_OPI();
+  ~GPIO_OPI() override;
   GPIO_OPI(const GPIO_OPI&) = delete;
   GPIO_OPI& operator=(const GPIO_OPI&) = delete;
-
-  ~GPIO_OPI() override;
 
   // GPIOBase interface
   void Output(const std::vector<size_t>& pins,
               const std::vector<size_t>& values) override;
   void ConfigureOutputPints(const std::vector<size_t>& pins) override;
+
+ private:
+  std::string gpio_chip_name_ = "gpiochip0";
+  using ChipPtr = std::unique_ptr<gpiod_chip, void (*)(gpiod_chip*)>;
+  ChipPtr chip_;
+  using PinPtr = std::unique_ptr<gpiod_line, void (*)(gpiod_line*)>;
+  std::unordered_map<size_t, PinPtr> pins_;
 };
 
 #endif  // GPIO_OPI_H
