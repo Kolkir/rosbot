f62fa3d4c3b8a104a54afca94afbd92bbadbe746
kolkir <rotate@ukr.net>
2021-02-25 20:10:33 +0200

    Add system instalation script for ROS melodic (Jetson)

---
+++ scripts/install_ide_melodic.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+sudo apt-get update
+sudo apt-get install -y qtcreator
+sudo apt-get install -y clang-format
+sudo apt-get install -y libclang-common-8-dev
+sudo apt-get install -y vim
+sudo apt-get install -y tmux
+sudo apt-get install -y mc
+sudo apt-get install -y git
+sudo apt-get install -y git-gui
+sudo apt-get install -y ros-melodic-video-stream-opencv
+sudo apt-get install -y ros-melodic-teleop-twist-keyboard
+sudo apt-get install -y ros-melodic-rosparam-shortcuts
+sudo apt-get install -y ros-melodic-rqt-robot-steering
+sudo apt-get install -y ros-melodic-gazebo-ros-pkgs
+sudo apt-get install -y ros-melodic-gazebo-ros-control
+sudo apt-get install -y ros-melodic-ros-controllers
+sudo apt-get install -y ros-melodic-genpy
+sudo apt-get install -y libgpiod-dev
+sudo apt-get install -y ros-melodic-costmap-2
+sudo apt-get install -y ros-melodic-octomap-msgs
+sudo apt-get install -y ros--octomap-server
+sudo apt-get install -y ros-melodic-amcl
+sudo apt-get install -y ros-melodic-move-base
+sudo apt-get install -y ros-melodic-pointcloud-to-laserscan
+sudo apt-get install -y libcv-bridge1d 
